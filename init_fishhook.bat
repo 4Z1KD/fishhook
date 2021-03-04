717fe50cab81fc5dd40233e79db6abdd07be38c5
Gil (4Z1KD) <4z1kd@iarc.org>
2021-03-04 09:33:21 +0200

    fishhook version 1.0.0

--- init_fishhook.bat
+++ init_fishhook.bat
@@ -1,3 +1,4 @@
+@echo off
 REM start cmd /k "activate & cd /d [PATH_TO_FOLDER] & python fishhook.py [WAV_FILE_NAME] [CHUNK_DURATION] [DELAY] [PTT_LAGGING]"
-start cmd /k "activate & cd /d C:\path\to\fishhook & python fishhook.py x.wav 60 5 0.5"
+start cmd /k "activate & cd /d C:\Users\gill\Documents\Projects\fishhook & python fishhook.py tx.wav 60 5 0.5"
 REM exit
