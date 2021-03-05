52e5587ad5ad0e316a502981c091924fc9650004
Gil (4Z1KD) <4z1kd@iarc.org>
2021-03-05 11:16:24 +0200

    Add files via upload

--- init_fishhook.bat
+++ init_fishhook.bat
@@ -1,4 +1,11 @@
 @echo off
 REM start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -h & python fishhook.py -a
-start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -f tx.wav -c 10 -d 5 -l 0.5 2 10"
+set FILE_NAME=tx.wav
+set /A CHUNK_DURATION=40
+set /A DELAY=5
+set /A PTT_LAG=0.5
+set /A INPUT_DEVICE_ID=2
+set /A OUTPUT_DEVICE_ID=10
+
+start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -f %FILE_NAME% -c %CHUNK_DURATION% -d %DELAY% -l %PTT_LAG% %INPUT_DEVICE_ID% %OUTPUT_DEVICE_ID%"
 exit
