b8998158da1456db8a2a095aaaee8baf8891d434
Gil (4Z1KD) <4z1kd@iarc.org>
2021-03-05 17:59:01 +0200

    Add files via upload

--- init_fishhook.bat
+++ init_fishhook.bat
@@ -9,8 +9,8 @@ set /A OUTPUT_DEVICE_ID=7
 
 REM if you want to trigger a physical ptt,
 REM you should set a COM port for the interface and add '--realptt' to command line
-set SERIAL_PORT=COM99
+set SERIAL_PORT=COM9
 set REAL_PTT=--realptt
 
-start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -f %FILE_NAME% -c %CHUNK_DURATION% -d %DELAY% -l %PTT_LAG% %INPUT_DEVICE_ID% %OUTPUT_DEVICE_ID%"
+start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -f %FILE_NAME% -c %CHUNK_DURATION% -d %DELAY% -l %PTT_LAG% -s %SERIAL_PORT% %INPUT_DEVICE_ID% %OUTPUT_DEVICE_ID% %REAL_PTT%"
 exit
