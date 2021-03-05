6ee906b3956fdc3a14535bd204f5c967c17d49e2
Gil (4Z1KD) <4z1kd@iarc.org>
2021-03-05 14:15:30 +0200

    Add files via upload

--- fishhook.py
+++ fishhook.py
@@ -1,12 +1,13 @@
 from pynput.keyboard import Key, Controller
 import time
+from serial.serialutil import SerialException
 import win32gui
-import sys
 import sounddevice as sd
 import soundfile as sf
 from colorama import init, Fore, Style
 import math
 import argparse
+import serial
 
 init()
 
@@ -25,6 +26,8 @@ parser.add_argument('-f', metavar='FILE_PATH', type=str, help='path to the .wav 
 parser.add_argument('-c', metavar='CHUNK_DURATION', type=int, help='chunk length (default: %(default)ss)', dest="chunk_duration", default=10)
 parser.add_argument('-d', metavar='DELAY', type=int, help='delay between chunck (default: %(default)ss)', dest="delay", default=5)
 parser.add_argument('-l', metavar='LAG', type=float, help='ptt lagging (default: %(default)ss)', dest="ptt_lagging", default=0.5)
+parser.add_argument('-s', metavar='COM[n]', type=str, help='serial port', dest="serial_port")
+parser.add_argument('--realptt', action='store_true', help='trigger a physical PTT over the serial port')
 args = parser.parse_args(remaining)
 
 sd.default.device = args.input_device_id,args.output_device_id
@@ -34,6 +37,16 @@ print(f"{Fore.GREEN}chunk duration:\t{args.chunk_duration}{Style.RESET_ALL}")
 print(f"{Fore.GREEN}delay:\t\t{args.delay}{Style.RESET_ALL}")
 print(f"{Fore.GREEN}ptt lagging:\t{args.ptt_lagging}{Style.RESET_ALL}")
 
+physical_port = False
+if args.realptt and args.serial_port:
+    try:
+        ser = serial.Serial(args.serial_port)
+        ser.setRTS(False)
+        physical_port = True
+    except SerialException:
+        physical_port = False
+        print(f"{Fore.YELLOW}failed to open '{args.serial_port}'. check device manager for the correct port.{Style.RESET_ALL}")        
+
 # Yield successive n-sized chunks from lst
 def chunks(lst, n):
     for i in range(0, len(lst), n):
@@ -42,23 +55,32 @@ def chunks(lst, n):
 handle = win32gui.FindWindow(None, "iaxRpt")
 try:
     if handle:
+        win32gui.SetForegroundWindow(handle)
+    else:
+        print(f"{Fore.YELLOW}could not find an instance of iaxRpt. did you forget to run it?{Style.RESET_ALL}")
+    
+    if handle or physical_port:
         # Extract data and sampling rate from a wav file
         data, fs = sf.read(args.wavfile, dtype='float32')
         data_chunks = chunks(data,fs*args.chunk_duration)
-        keyboard = Controller()
-        win32gui.SetForegroundWindow(handle)        
+        keyboard = Controller()    
         i = 0
         for chunk in data_chunks:
             print(f"playing {int(math.ceil(i*args.chunk_duration + len(chunk)/fs))}/{int(math.ceil(len(data)/fs))} sec", end ="\r") 
             i+=1
+            if physical_port:
+                ser.setRTS(True)
             keyboard.press(Key.ctrl)
             time.sleep(args.ptt_lagging)
             sd.play(chunk, fs)
             status = sd.wait()  # Wait until file is done playing
             keyboard.release(Key.ctrl)
+            if physical_port:
+                ser.setRTS(False)
             time.sleep(args.delay)
     else:
-        print(f"{Fore.LIGHTRED_EX}could not find an instance of iaxRpt. did you forget to run it?{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTRED_EX}both physical interface and iaxRpt are not available{Style.RESET_ALL}")
+    
 except KeyboardInterrupt:
     print('\nInterrupted by user')
 except Exception as e:
--- init_fishhook.bat
+++ init_fishhook.bat
@@ -4,8 +4,13 @@ set FILE_NAME=tx.wav
 set /A CHUNK_DURATION=40
 set /A DELAY=5
 set /A PTT_LAG=0.5
-set /A INPUT_DEVICE_ID=2
-set /A OUTPUT_DEVICE_ID=10
+set /A INPUT_DEVICE_ID=1
+set /A OUTPUT_DEVICE_ID=7
+
+REM if you want to trigger a physical ptt,
+REM you should set a COM port for the interface and add '--realptt' to command line
+set SERIAL_PORT=COM99
+set REAL_PTT=--realptt
 
 start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -f %FILE_NAME% -c %CHUNK_DURATION% -d %DELAY% -l %PTT_LAG% %INPUT_DEVICE_ID% %OUTPUT_DEVICE_ID%"
 exit
