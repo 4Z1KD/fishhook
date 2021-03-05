62aa6ed4002f3b391662d3d55c506cade28e23fd
Gil (4Z1KD) <4z1kd@iarc.org>
2021-03-05 10:22:39 +0200

    Add files via upload

--- init_fishhook.bat
+++ init_fishhook.bat
@@ -1,4 +1,4 @@
 @echo off
-REM start cmd /k "activate & cd /d C:\[PATH_TO_PROJECT_FOLDER]\fishhook & python fishhook.py -h & python fishhook.py -f x.wav -c 11 -d 6 -l 0.5 2 10"
-start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -h & python fishhook.py -f x.wav -c 11 -d 6 -l 0.5 2 10"
+start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -h & python fishhook.py -a
+start cmd /k "activate & cd /d C:\Users\Gil\Documents\Projects\fishhook & python fishhook.py -f tx.wav -c 10 -d 5 -l 0.5 2 10"
 exit
